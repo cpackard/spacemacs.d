c42702c8bbb863002473684b50c6b6cdcb7dcbb0
cpackard <christianpackard@10-18-61-166.dynapool.wireless.nyu.edu>
2022-06-08 18:04:33 -0400

    fix lag on keystroke in python projects using virtualenv

--- init.el
+++ init.el
@@ -130,6 +130,15 @@ This function should only modify configuration layer settings."
           lsp-ui-sideline-enable nil                      ; sidebar code actions visual indicator
           treemacs-space-between-root-nodes nil           ; spacing in treemacs views
           lsp-log-io t                                    ; Log client-server json communication
+          lsp-ui-doc-enable nil       ;; disable all doc popups
+          lsp-ui-sideline-enable nil  ;; disable sideline bar for less distraction
+          treemacs-space-between-root-nodes nil  ;; no spacing in treemacs views
+          :config
+          (lsp-register-custom-settings
+           '(("pyls.plugins.pyls_mypy.enabled" t t)
+             ("pyls.plugins.pyls_mypy.live_mode" t t)
+             ("pyls.plugins.pyls_black.enabled" t t)
+             ("pyls.plugins.pyls_isort.enabled" t t)))
           )
 
      markdown
@@ -157,12 +166,14 @@ This function should only modify configuration layer settings."
 
      (python :variables
              python-backend 'lsp
-             python-lsp-server 'pyright
+             python-lsp-server 'pylsp
              python-test-runner 'pytest
              python-formatter 'black
              python-format-on-save t
              python-save-before-test t
              python-fill-column 120
+             python-auto-set-local-pyenv-version 'on-project-visit
+             python-auto-set-local-pyvenv-virtualenv 'on-project-visit
              python-sort-imports-on-save t)
 
      ;; Text-based file manager with preview - SPC a t r r
@@ -763,6 +774,9 @@ before packages are loaded."
 
   ;; dotspacemacs/user-config divided into files
   ;; - comment files to skip loading specific configuration
+  ;; Python
+  (with-eval-after-load 'flycheck
+    (flycheck-add-next-checker 'python-flake8 '(t . python-mypy)))
 
   ;; General configuration
   (setq user-config-file (file-truename (concat dotspacemacs-directory "user-config.el")))
