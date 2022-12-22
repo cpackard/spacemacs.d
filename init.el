8022c3d9c57502ba2d4359fb385a3a63ff07dd50
Christian Packard <6462409+cpackard@users.noreply.github.com>
2022-12-22 12:40:37 -0500

    fix folding method for python

--- init.el
+++ init.el
@@ -651,7 +651,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Code folding method. Possible values are `evil', `origami' and `vimish'.
    ;; (default 'evil)
-   dotspacemacs-folding-method 'vimish
+   dotspacemacs-folding-method 'evil
 
    ;; If non-nil and `dotspacemacs-activate-smartparens-mode' is also non-nil,
    ;; `smartparens-strict-mode' will be enabled in programming modes.
